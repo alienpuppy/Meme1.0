140964524fb3daf569407aaa18c6a944e87d1a6b
mary mcmanamon <marymcmanamon@mary-mcmanamons-iMac.local>
2016-01-23 20:53:28 -0800

    Add Meme1.0 debugger file

---
+++ Meme1.0/Meme.swift
@@ -0,0 +1,9 @@
+//
+//  Meme.swift
+//  Meme1.0
+//
+//  Created by mary mcmanamon on 1/17/16.
+//  Copyright © 2016 mary mcmanamon. All rights reserved.
+//
+
+import Foundation
---
+++ Meme1.0/MemeTextFieldDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  MemeTextFieldDelegate.swift
+//  Meme1.0
+//
+//  Created by mary mcmanamon on 1/17/16.
+//  Copyright © 2016 mary mcmanamon. All rights reserved.
+//
+
+import Foundation
